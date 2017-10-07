ef9574425734c39e72852b1b7523cd84159184e1
Gabriel Quadrado <gabriel.quadrado@monitoratec.com.br>
2017-10-07 16:04:44 -0300

    Primeiro commit

---
+++ src/main/java/com/opensanca/trilharest/filmes/hello/HelloController.java
@@ -0,0 +1,18 @@
+package com.opensanca.trilharest.filmes.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by quadrado on 07/10/2017.
+ */
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @RequestMapping(path = "", method = {RequestMethod.GET})
+    public String hello(){
+        return "Olá!";
+    }
+}
